6077e4c7ed8f590db7ca9f31ddba2a7563b044b4
Jonathan Bohren <jbo@jhu.edu>
2016-02-04 14:36:24 -0500

    cleaning up param sync

--- param_sync_fkie/scripts/param_sync.py
+++ param_sync_fkie/scripts/param_sync.py
@@ -5,23 +5,26 @@ import rospy
 from multimaster_msgs_fkie.msg import MasterState
 
 def master_changed(msg, cb_args):
-    print msg
     master_proxies, param_cache = cb_args
     master_proxies[msg.master.name] = rospy.MasterProxy(msg.master.uri)
 
     for name_from, master_from in master_proxies.items():
         for name_to, master_to in master_proxies.items():
             if name_to != name_from:
-                rospy.loginfo("Getting params from...".format(name_from))
+                rospy.logdebug("Getting params from...".format(name_from))
                 params_from = master_from['/']
-                del params_from['/_']
-                rospy.loginfo("Syncing params from {} to {}...".format(name_from, name_to))
+                rospy.logdebug("Got {} params.".format(len(params_from)))
+                if '_' in params_from:
+                    del params_from['_']
+                if '/_' in params_from:
+                    del params_from['/_']
+                rospy.logdebug("Syncing params from {} to {}...".format(name_from, name_to))
                 if param_cache.get(name_from, None) != params_from:
                     param_cache[name_from] = params_from
                     master_to['/_/'+name_from] = params_from
-                    rospy.loginfo("Done syncing params from {} to {}.".format(name_from, name_to))
+                    rospy.logdebug("Done syncing params from {} to {}.".format(name_from, name_to))
                 else:
-                    rospy.loginfo("Params have not changed from {} to {}.".format(name_from, name_to))
+                    rospy.logdebug("Params have not changed from {} to {}.".format(name_from, name_to))
 
 
 def main():
